33908f90ccf1ba7fa28508562096554459fd35a6
Nicolai-Schultz <nicoschultz42@gmail.com>
2024-11-13 14:05:32 +0100

    Changes to exercies

--- Main/exercises.c
+++ Main/exercises.c
@@ -4,54 +4,88 @@
 //  Test placeholders - dette kommer fra questionnaire
 double body_weight = 74;
 double rep_max_pushup = 30;
+double rep_max_squats =
 
 //  All exercises funktion:
 int main(void) {
 
     //  With equipment
-    equipment_2 bench_press_equipment;
-    bench_press_equipment.bench = 1;
-    bench_press_equipment.barbell = 1;
+    //  Bench press
+    check_equipment bench_press_equipment =
+        {0, 1, 1, 0, 0, 0};
 
-    exercise bench_press = {"Bench press", bench_press_equipment, 2.5, base_weight(body_weight,rep_max_pushup)};
-    exercise weighted_squats = {"Weighted squats", barbell, 2.5, base_weight(body_weight,rep_max_pushup)};
-    exercise lateral_pulldown = {"Bench press", barbell, 2.5, base_weight(body_weight,rep_max_pushup)};
-    //  Without equipment
-    exercise push_ups = {"Bench press", barbell, 2.5, base_weight(body_weight,rep_max_pushup)};
-    exercise squats = {"Bench press", barbell, 2.5, base_weight(body_weight,rep_max_pushup)};
-    //  What should be an unweighted exercise for the back?
-    exercise back_push_ups = {"Bench press", barbell, 2.5, base_weight(body_weight,rep_max_pushup)};
+    exercise bench_press = {"Bench press", bench_press_equipment, 2.5, base_weight_bench_press(body_weight,rep_max_pushup)};
+
+    //  Weighted squats
+    check_equipment weighted_squats_equipment =
+    {0, 1, 0, 0, 0, 0};
+    exercise weighted_squats = {"Weighted squats", weighted_squats_equipment, 2.5, base_weight_weighted_squats(body_weight,rep_max_pushup)};
 
     print_exercise(bench_press);
 }
 
 
 
-double base_weight(double body_weight, double rep_max_pushup) {
+double base_weight_bench_press(double body_weight, double rep_max_pushup) {
     //  Multiplying by a factor so amount of weight based on 12 reps is found
-    double rep_factor = 0.71;
+    double rep_factor_12 = 0.71;
     //  Multiply by 0.65 since that is the percentage of bodyweigth lifted during a pushup
-    body_weight *= 0.65;
+    double body_weight_pushup = body_weight* 0.65;
     //  Mayhews calculation for 1rm:
-    double calculation_1rm = (100 * body_weight)/(52.2+41.9*pow(M_E,(-0.055*rep_max_pushup)));
+    double calculation_1rm_bench_press = (100 * body_weight_pushup)/(52.2+41.9*pow(M_E,(-0.055*rep_max_pushup)));
 
     //  Returning modified calculation
-    if(calculation_1rm % 2 == 0) {
+    /*
+    if(calculation_1rm_bench_press % 2 == 0) {
 
-    }
-    return calculation_1rm * rep_factor;
+    }*/
+    return calculation_1rm_bench_press * rep_factor_12;
+}
+
+double base_weight_weighted_squats(double body_weight, double rep_max_pushup) {
+    //  Multiplying by a factor so amount of weight based on 12 reps is found
+    double rep_factor_12 = 0.71;
+    //  Multiply by 0.65 since that is the percentage of bodyweigth lifted during a pushup
+    double body_weight_pushup = body_weight* 0.65;
+    //  Mayhews calculation for 1rm:
+    double calculation_1rm_bench_press = (100 * body_weight_pushup)/(52.2+41.9*pow(M_E,(-0.055*rep_max_pushup)));
+
+    //  Returning modified calculation
+    /*
+    if(calculation_1rm_bench_press % 2 == 0) {
+
+    }*/
+    return calculation_1rm_bench_press * rep_factor_12;
 }
 
 void print_exercise(exercise exercise) {
     //få værdierne i struct ud
     char *name = exercise.name;
-    int needed_equipment = exercise.needed_equipment;
+    //int equipment_required = exercise.necessary_equipment;
     double addition = exercise.addition;
     double base_weight = exercise.base_weight;
 
     //print værdierne
     printf("Name: %s\n", name);
-    printf("Needed equipment: %d\n", needed_equipment);
+    printf("Needed equipment: \n");
+    if (exercise.necessary_equipment.nothing == 1) {
+        printf(" - Nothing\n");
+    }
+    if (exercise.necessary_equipment.barbell == 1) {
+        printf(" - Barbell\n");
+    }
+    if (exercise.necessary_equipment.bench == 1) {
+        printf(" - Bench\n");
+    }
+    if (exercise.necessary_equipment.pull_up_bar == 1) {
+        printf(" - Pull up bar\n");
+    }
+    if (exercise.necessary_equipment.pull_down_machine == 1) {
+        printf(" - Pull down machine\n");
+    }
+    if (exercise.necessary_equipment.resistance_bands == 1) {
+        printf(" - Resistance bands\n");
+    }
     printf("Addition: %lf\n", addition);
     printf("base weight: %lf\n", base_weight);
     printf("\n");
--- Main/exercises.h
+++ Main/exercises.h
@@ -1,24 +1,30 @@
-double base_weight(double body_weight, double rep_max_pushup);
+double base_weight_bench_press(double body_weight_pushups, double rep_max_pushup);
+double base_weight_weighted_squats(double body_weight, double rep_max_pushup);
 
 //  Stages from problem analysis
 enum stage { begynder, let_øvet, øvet, meget_øvet, ekspert };
 enum equipment { nothing, barbell, bench, pull_up_bar, pull_down_machine, resistance_bands, length_of_equipment_enum };
+// nothing, barbell, bench, pull_up_bar, pull_down_machine, resistance_bands
+typedef struct {
+    int nothing;
+    int barbell;
+    int bench;
+    int pull_up_bar;
+    int pull_down_machine;
+    int resistance_bands;
+} check_equipment;
 
 //  Exercise platform
 typedef struct {
     char name[20];
-    //  Ændr til ny type
-    enum equipment needed_equipment;
+    //  When calling necessary_equipment.x it searches in the check_equipment struct
+    check_equipment necessary_equipment;
     //  We assume that weight only increases by 2.5, argue for this in implementation/design
     double addition;
     double base_weight;
 } exercise;
 
-typedef struct {
-    int bench = 0;
-    int barbell = 0;
-    int pull_up_bar = 0;
-} equipment_2;
+
 
 
 exercise* create_all_exercises();
