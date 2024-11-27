c57acec85b58a234e8cd93cacb5751768508349d
Jesp <87063030+JespJesp@users.noreply.github.com>
2024-11-27 14:43:29 +0100

    Update

--- Main/workout_session.c
+++ Main/workout_session.c
@@ -17,66 +17,72 @@ enum day_of_the_week {
 typedef struct {
     exercise exercises[32];
     muscle_group fatigued_muscle_groups[32];
-    double duration;
+    double duration; // in minutes
     enum day_of_the_week day;
 } workout;
 
 
-
-
 /// WARNING: Allocates memory using malloc(). Remember to dispose of the memory.
-/// SUMMARY: Creates an array of workouts.
-/// Takes into account:
-/// 1) available time per workout.
-/// 2) available workouts per week.
-/// 3) junk volume
-/// todo: Find information about HIIT max sets.
-/// (resistance training: no more than 6 sets per muscle group per workout).
-/// 4) recovery time
-/// (resistance training: 72 hours per muscle group for beginners, 48 hours for more advanced trainees).
-/// todo: Find information about HIIT recovery time.
-/// @param questionaire the used questionaire.
-/// @param resistance array of resistance training exercises to be used.
-/// @param aerobic array of aerobic training exercises to be used.
+/// SUMMARY: Creates an array of workouts whilst taking into account:
+/// 1) Available time per workout.
+/// 2) Available workouts per week.
+/// 3) Junk volume.
+/// 4) Recovery time.
+/// @param questionaire - the used questionaire.
+/// @param resistance - array of resistance training exercises to be used.
+/// @param aerobic - array of aerobic training exercises to be used.
 workout *create_workouts(questionaire questionaire, exercise resistance[], int resistance_length, exercise aerobic[], int aerobic_length) {
 
-    /* Ideas:
-     * - Look at each muscle group and add an exercise that does that (only one set)
-     * - When the workout array has been filled out with these, increase reps until max workout time is reached
-     *
-     */
-
-    //My idea is that, for resistance training, you must warm up for each exercise (do a warmup set),
-    //because resistance training puts a lot of strain on specific muscle groups,
-    //whereas aerobic training is more focused on straining the respiratory system,
-    //so a general warmup is good enough for it.
-    //So, I should find information about this on the internet.
-
-    //todo: implement changes based on fitness level
-    int weekly_sets_limit = 20; //todo: add explanation in report
-    int daily_sets_limit = 6;
-    int resistance_recovery_time_in_days = 2;
-    int aerobic_recovery_time_in_days = ?; //todo: HIIT recovery time
-    if (questionaire.fitness_level >= advanced_beginner) { // novice = 5, advanced_beginner = 4, competent = 3, proficient = 2
-        weekly_sets_limit = 10;  //todo: add explanation in report
-        daily_sets_limit = 5; //todo: add explanation in report
-        resistance_recovery_time_in_days = 3;
-        aerobic_recovery_time_in_days = ?; //todo: HIIT recovery time
-    }
-    if (questionaire.fitness_level >= novice) {
-        daily_sets_limit = 4; //todo: add explanation in report
+    // Get workout rules (as defined in our P1 report)
+    int resistance_weekly_sets_limit;
+    int resistance_daily_sets_limit = 6; // any more sets are junk volume
+    int resistance_recovery_time_in_days;
+    int max_hiit_sessions_per_week;
+    int aerobic_set_rest_multiplier;
+    int general_warmup_duration = 5; // in minutes
+    enum fitness_level level = questionaire.fitness_level;
+    if (level >= novice) { // novice = 5, advanced_beginner = 4, competent = 3, proficient = 2
+        resistance_weekly_sets_limit = 10; // to make it easier to get into the habit of fitness
+        resistance_recovery_time_in_days = 3; // recovery time ranges from 48-72 hours for 6-15 reps
+        max_hiit_sessions_per_week = 3; // limited, because beginners are recommended 1-3 sessions per week
+        aerobic_set_rest_multiplier = 3; // 1:3 work-to-rest ratio
+    } else if (level >= advanced_beginner) {
+        resistance_weekly_sets_limit = 10; // to make it easier to get into the habit of fitness
+        resistance_recovery_time_in_days = 3; // recovery time ranges from 48-72 hours for 6-15 reps
+        max_hiit_sessions_per_week = 7; // no limit, because recovery time is 24 hours
+        aerobic_set_rest_multiplier = 2; // 1:2 work-to-rest ratio
+    } else {
+        resistance_weekly_sets_limit = 20; //todo: add explanation (I should research what the max volume should be)
+        resistance_recovery_time_in_days = 2; // recovery time ranges from 48-72 hours for 6-15 reps
+        max_hiit_sessions_per_week = 7; // no limit, because recovery time is 24 hours
+        aerobic_set_rest_multiplier = 1; // 1:1 work-to-rest ratio
     }
 
-    // Set workouts per week
-    int workouts_per_week = sizeof(questionaire.available_workout_days) / sizeof(questionaire.available_workout_days[0]); //todo is enum day_of_the_week
+    // Allocate amount of workouts per week
+    int workouts_per_week = sizeof(questionaire.available_workout_days) / sizeof(questionaire.available_workout_days[0]); //todo: is enum day_of_the_week
     workout* workouts = malloc(sizeof(workout) * workouts_per_week);
     for (int i = 0; i < workouts_per_week; i++ ) {
-        workouts[i].duration = 0;
+        workouts[i].duration = general_warmup_duration; // there is always included a general warm up in the workout
         workouts[i].day = questionaire.available_workout_days[0];
     }
 
-    // Add exercises to workouts
-    //todo: do stuff
+
+    // Get resistance training days
+
+
+
+    // Add resistance training exercises
+
+
+    // Add aerobic training exercises
+
+    // Fill workouts with sets
+
+
+    // Reverse order of exercises (makes aerobic exercises come first)
+
+
+
 
     return workouts;
 }
