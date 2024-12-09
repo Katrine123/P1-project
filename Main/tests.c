4afd42661a9fd50eda3e9fa8964b3777f90d1e47
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2024-12-09 10:52:19 +0100

    more stuff

--- Main/tests.c
+++ Main/tests.c
@@ -5,7 +5,43 @@
 #include "tools.h"
 
 TEST_CASE(questionnaire_test1,{
-    FILE *test_file = fopen("user_input.txt","r");
+    FILE *test_file = fopen("user_input.txt","w");
+        if (test_file == NULL) {
+            printf("The file couldn't be opened");
+            exit(-1);
+        }
+    fprintf(test_file,"67 male 72 183 6 8 1 4 -1 120 Yes");
+    fclose(test_file);
+    test_file = fopen("user_input.txt","r");
+        if (test_file == NULL) {
+            printf("The file couldn't be opened");
+            exit(-1);
+        }
+    questionnaire user_test = create_and_answer_questionnaire(test_file);
+    fclose(test_file);
+    CHECK_EQ_INT(user_test.age,67);
+    CHECK_EQ_STRING(user_test.gender,"male");
+    CHECK_EQ_DOUBLE(user_test.weight,72,0.001);
+    CHECK_EQ_DOUBLE(user_test.height,183,0.001);
+    CHECK_EQ_INT(user_test.pushups,6);
+    CHECK_EQ_INT(user_test.squats,8);
+    CHECK_EQ_INT(user_test.fitness_level,1);
+    CHECK_EQ_INT(user_test.training_days[0].day_week,thursday);
+    CHECK_EQ_DOUBLE(user_test.training_days[0].available_time,120,0.001);
+    for(int i = 0; i<length_of_equipment_enum;i++) {
+        CHECK_EQ_INT(user_test.available_equipment[i],1);
+    }
+})
+
+TEST_CASE(questionnaire_test2,{
+    FILE *test_file = fopen("user_input.txt","w");
+        if (test_file == NULL) {
+            printf("The file couldn't be opened");
+            exit(-1);
+        }
+    fprintf(test_file,"67 male 72 183 6 8 1 4 -1 120 Yes");
+    fclose(test_file);
+    test_file = fopen("user_input.txt","r");
         if (test_file == NULL) {
             printf("The file couldn't be opened");
             exit(-1);
@@ -14,50 +50,7 @@ TEST_CASE(questionnaire_test1,{
     fclose(test_file);
 })
 
-TEST_CASE(questionnaire_test2,{
-    FILE *test_file2 = fopen("user_input2.txt","r");
-        if (test_file2 == NULL) {
-            printf("The file couldn't be opened");
-            exit(-1);
-        }
-    questionnaire user_test = create_and_answer_questionnaire(test_file2);
-    fclose(test_file2);
-})
-
 MAIN_RUN_TESTS(questionnaire_test1,questionnaire_test2)
-
-/*int main(void) {
-    FILE *test_file = fopen("user_input.txt","w");
-    if (test_file == NULL) {
-        printf("The file couldn't be opened");
-        exit(-1);
-    }
-    fprintf(test_file, "67 male 72 183 6 8 1 4 -1 120 Yes\n");
-    fclose(test_file);
-    test_file = fopen("user_input.txt","r");
-    if (test_file == NULL) {
-        printf("The file couldn't be opened");
-        exit(-1);
-    }
-
-    questionnaire user_test = create_and_answer_questionnaire(test_file);
-    //Shit works, so now actually test stuff
-    assert(user_test.age == 67);
-    assert(strcmp(user_test.gender,"male")==0);
-    assert(user_test.weight == 72);
-    assert(user_test.height == 183);
-    assert(user_test.pushups == 6);
-    assert(user_test.squats == 8);
-    assert(user_test.fitness_level == 1);
-    assert(user_test.training_days->day_week == thursday);
-    assert(user_test.training_days->available_time == 120);
-
-    assert(test(4,5)==9);
-    fclose(test_file);
-    return 0;
-}*/
-
-/*Anything with scanf is hard af to test*/
 
 /*What to test:
  * Sorting algoritm
