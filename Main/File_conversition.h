94f3b1752859308427fc44212d46d516e564fc37
FreshLuna <msanda24@student.aau.dk>
2024-12-11 01:08:51 +0100

    KIG HER STORE ÆNDRINGER

---
+++ Main/File_conversition.h
@@ -0,0 +1,13 @@
+//
+// Created by FreshLuna on 12/10/2024.
+//
+
+#ifndef FILE_CONVERSITION_H
+#define FILE_CONVERSITION_H
+char *int_to_str(int n);
+int str_to_int(const char *str);
+
+char *double_to_str(double d);
+double str_to_double(const char *str);
+
+#endif //FILE_CONVERSITION_H
