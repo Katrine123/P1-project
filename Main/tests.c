8d9e0eb803da5863ae239902572c6c8254d07b00
Katrine123 <113664220+Katrine123@users.noreply.github.com>
2024-12-11 19:29:33 +0100

    mere test og ouline af main

--- Main/tests.c
+++ Main/tests.c
@@ -4,6 +4,7 @@
 #include "mtest.h"
 #include "tools.h"
 
+#pragma region questionnaire
 TEST_CASE(questionnaire_test1,{
     //Making "User inputs"
     FILE *test_file = fopen("user_input.txt","w");
@@ -52,7 +53,9 @@ TEST_CASE(questionnaire_test2,{
     fclose(test_file);
 })
 
+#pragma endregion
 
+#pragma region homemade_scan
 TEST_CASE(homemade_scan_test,{//OPLEVER FEJL HVIS ANDEN COMPUTER. DONT KNOW WHY :]
     FILE *test_file = fopen("user_input.txt","w");
         if (test_file == NULL) {
@@ -80,7 +83,11 @@ TEST_CASE(homemade_scan_test,{//OPLEVER FEJL HVIS ANDEN COMPUTER. DONT KNOW WHY 
     CHECK_EQ_DOUBLE(double_test,34,0.001);
     fclose(test_file);
 })
+#pragma endregion
 
+
+#pragma region sorting_exercises
+//TEST THAT YOU ONLY GET THE EXERCISES NEEDING NO EQUIPMENT - IS SUPPOSED TO SUCCEED
 TEST_CASE(sorting_exercises_test,{
     questionnaire test = {67,"male",72,183,7,12,1,{monday,67},{0,0,0,0,0}};
     exercise ex_test[length_of_exercises_list];
@@ -99,12 +106,86 @@ TEST_CASE(sorting_exercises_test,{
     free(exercise_list_sorted);
 })
 
+//TESTS THAT YOU GET ALL EXERCISES IF YOU HAVE ALL EQUIPMENT - IS SUPPOSED TO SUCCEED
+TEST_CASE(sorting_exercises_test2,{
+    questionnaire test = {67,"male",72,183,7,12,1,{monday,67},{1,1,1,1,1}};
+    exercise ex_test[length_of_exercises_list];
+    resistance_exercises_list(ex_test, test);
+    aerobic_exercises_list(ex_test,test);
+    int sorted_count = 0;
+    exercise* exercise_list_sorted = create_available_exercises(ex_test, test, &sorted_count);
+    CHECK_EQ_STRING(exercise_list_sorted[0].name,"Bench press");
+    CHECK_EQ_STRING(exercise_list_sorted[1].name,"Weighted squats");
+    CHECK_EQ_STRING(exercise_list_sorted[2].name,"Air squats");
+    CHECK_EQ_STRING(exercise_list_sorted[3].name,"Pushups");
+    CHECK_EQ_STRING(exercise_list_sorted[4].name,"Elevated pushups");
+    CHECK_EQ_STRING(exercise_list_sorted[5].name,"Burpees");
+    CHECK_EQ_STRING(exercise_list_sorted[6].name,"Jumping jacks");
+
+    // Free the dynamically allocated memory for the sorted list
+    free(exercise_list_sorted);
+})
+
+//TESTS FOR -1 - IS SUPPOSED TO FAIL
+TEST_CASE(sorting_exercises_test3,{
+    questionnaire test = {67,"male",72,183,7,12,1,{monday,67},{-1,1,1,1,1}};
+    exercise ex_test[length_of_exercises_list];
+    resistance_exercises_list(ex_test, test);
+    aerobic_exercises_list(ex_test,test);
+    int sorted_count = 0;
+    exercise* exercise_list_sorted = create_available_exercises(ex_test, test, &sorted_count);
+    CHECK_EQ_STRING(exercise_list_sorted[0].name,"Bench press");
+    CHECK_EQ_STRING(exercise_list_sorted[1].name,"Weighted squats");
+    CHECK_EQ_STRING(exercise_list_sorted[2].name,"Air squats");
+    CHECK_EQ_STRING(exercise_list_sorted[3].name,"Pushups");
+    CHECK_EQ_STRING(exercise_list_sorted[4].name,"Elevated pushups");
+    CHECK_EQ_STRING(exercise_list_sorted[5].name,"Burpees");
+    CHECK_EQ_STRING(exercise_list_sorted[6].name,"Jumping jacks");
+
+    // Free the dynamically allocated memory for the sorted list
+    free(exercise_list_sorted);
+})
+#pragma endregion
+
+#pragma region calculations
+
 TEST_CASE(calculations,{
-    questionnaire test = {67,"male",65,183,5,12,1,{monday,67},{0,0,0,0,0}};
+    questionnaire test;
+    test.weight = 65;
+    test.pushups = 5;
     double test_value = base_weight_bench_press(test);
     CHECK_EQ_DOUBLE(test_value,35,0.001);
 })
+
+//IS SUPPOSED TO FAIL - THE QUESTIONNAIRE STOPS ANY ZEROS FOR WEIGHT
+TEST_CASE(calculations2,{
+    questionnaire test;
+    test.weight = 0;
+    test.pushups = 5;
+    double test_value = base_weight_bench_press(test);
+    CHECK_EQ_DOUBLE(test_value,30,0.001);
+})
+
+//TESTS FOR THE HIGHEST POSSIBLE WEIGHT - SHOULD SUCCEED
+TEST_CASE(calculations3,{
+     questionnaire test;
+     test.weight = 300;
+     test.pushups = 5;
+     double test_value = base_weight_bench_press(test);
+     CHECK_EQ_DOUBLE(test_value,162.5,0.001);
+})
+
+//TESTS FOR THE HIGHEST POSSIBLE WEIGHT - SHOULD SUCCEED
+TEST_CASE(calculations4,{
+     questionnaire test;
+     test.weight = 65;
+     test.pushups = 5000000;
+     double test_value = base_weight_bench_press(test);
+     CHECK_EQ_DOUBLE(test_value,55,0.001);
+})
 //EDGECASES!
+
+#pragma endregion
 
 TEST_CASE(upgrade_test,{
     questionnaire user_test = {67,"male",65,183,5,12,1,{monday,67},{1,1,1,1,1}};
@@ -132,7 +213,7 @@ TEST_CASE(upgrade_test,{
     CHECK_EQ_DOUBLE(ex_test[6].amount_of_reps,16,0.001);
 })
 
-MAIN_RUN_TESTS(/*questionnaire_test1,questionnaire_test2,homemade_scan_test, sorting_exercises_test, calculations,upgrade_test*/)
+MAIN_RUN_TESTS(calculations,calculations2,calculations3,calculations4)
 
 /*Testing af Jespers ting*/
 /*Update routine workout*/
