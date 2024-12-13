dffbafc23c47aab686c09a054c4dc74b0c8042a3
Nicolai-Schultz <nicoschultz42@gmail.com>
2024-12-13 09:24:42 +0100

    indtil videre

--- Main/tools.h
+++ Main/tools.h
@@ -98,8 +98,8 @@ exercise* create_all_exercises();
 void print_exercise(exercise exercises_list[]);
 void print_exercises_2(exercise sorted_exercise_list[], int count, questionnaire user, exercise exercises_list[]);
 void upgrade_downgrade(exercise *exercise_list_sorted, questionnaire user, int sorted_count, int *input);
-void upgrade_function(exercise *exercise_list_sorted, questionnaire user, int sorted_count, int input[]);
-//
+void upgrade_function(exercise *exercise_list_sorted, int input);
+void downgrade_function(exercise *exercise_list_sorted, int input);
 // Created by mwsan on 11/14/2024.
 //
 
--- Main/upgrade_downgrade.c
+++ Main/upgrade_downgrade.c
@@ -5,61 +5,65 @@
 
 
 void upgrade_downgrade(exercise *exercise_list_sorted, questionnaire user, int sorted_count, int *input) {
-    for (int i = 0; i < sorted_count; i++) {
-        input[i] = 0;
-    }
-    char answer;
-    printf("Did you complete all assigned exercises? (y/n)");
-    scanf(" %c", &answer);
-
-    if (answer == 'y') {
-        for (int i = 0; i < sorted_count; i++) {
-            input[i] = 1;
-            //  Call upgrade function
-        }
-        upgrade_function(exercise_list_sorted,user,sorted_count,input);
-
-    } else if (answer == 'n') {
-        printf("Which exercises did you complete?\n");
-        // print all the exercises available to the user
-        for (int i = 0; i < sorted_count; i++) {
-            //  Exercises are listed as 0-6, if they were listed from 1-7 it would provide issues..
-            printf("Exercise %d: %s\n", i, exercise_list_sorted[i].name);
-        }
-        int i = 0;
-        int input_temp;
-        do{
-            //TAGER IKKE HØJDE FOR -1 ELLER MIN MAX
-
-            scanf("%d", &input_temp);
-            if(input_temp < -1 || input_temp > sorted_count) {
-                printf("Please type values between 0 and %d. Unless you are done, then type -1. Enter values again! \n", sorted_count);
-                //  If condition is met restart the loop:
-                continue;
+    printf("\n Let's evaluate the difficulty of your exercises: \n");
+    for(int i = 0; i < sorted_count; i++) {
+        char answer;
+        char second_answer;
+        *input = i;
+        printf("Exercise %d: %s \n", i, exercise_list_sorted[i].name);
+        printf("Would you like to upgrade or downgrade? (u/d) \n");
+        scanf(" %c", &answer);
+        //  Go to, initial call. Happens if the user types a wrong char.
+        start:
+        if(answer == 'u') {
+            do {
+                upgrade_function(exercise_list_sorted, *input);
+                if(exercise_list_sorted[i].is_body_weight_exercise == 1) {
+                    printf("Are these repetitions all right? %lf (y/n) \n", exercise_list_sorted[i].amount_of_reps);
+                } else {
+                    printf("Is this weight all right? %lf (y/n) \n", exercise_list_sorted[i].base_weight);
+                }
+                scanf(" %c", &second_answer);
             }
-            if(input_temp!=-1) {
-                //  input_temp - 1
-                input[input_temp] = 1;
-                i++;
+            while(second_answer == 'n');
+        } else if (answer == 'd') {
+            // Repeated code except it downgrades!
+            int fail = 0;
+            do {
+                if(exercise_list_sorted[i].base_weight < 1 || exercise_list_sorted[i].amount_of_reps < 1) {
+                    printf("Not possible");
+                    break;
+                }
+                downgrade_function(exercise_list_sorted, *input);
+                if(exercise_list_sorted[i].is_body_weight_exercise == 1) {
+                    printf("Are these repetitions all right? %lf (y/n) \n", exercise_list_sorted[i].amount_of_reps);
+                } else {
+                    printf("Is this weight all right? %lf (y/n) \n", exercise_list_sorted[i].base_weight);
+                }
+                scanf(" %c", &second_answer);
             }
-
-        } while (input_temp != -1);
-        printf("TEST");
-        upgrade_function(exercise_list_sorted,user,sorted_count,input);
+            while(second_answer == 'n');
+        } else {
+            printf("Type either 'u' or 'd' !");
+            goto start;
+        }
     }
 }
 
-void upgrade_function(exercise *exercise_list_sorted, questionnaire user, int sorted_count, int input[]) {
-    printf("TEST3");
-    for (int i = 0; i < sorted_count; i++) {
-        if (input[i] == 1) {
-            //  What if the user weight is equal to bench press weight?
-                //  This has to be for body_weight_exercises
-            if(exercise_list_sorted[i].is_body_weight_exercise == 1) {
-                exercise_list_sorted[i].amount_of_reps += exercise_list_sorted[i].addition;
-            } else {
-                exercise_list_sorted[i].base_weight += exercise_list_sorted[i].addition;
-            }
-        }
+void upgrade_function(exercise *exercise_list_sorted, int input) {
+    if(exercise_list_sorted[input].is_body_weight_exercise == 1) {
+        exercise_list_sorted[input].amount_of_reps += exercise_list_sorted[input].addition;
+    } else {
+        exercise_list_sorted[input].base_weight += exercise_list_sorted[input].addition;
+    }
+
+
+}
+
+void downgrade_function(exercise *exercise_list_sorted, int input) {
+    if(exercise_list_sorted[input].is_body_weight_exercise == 1) {
+        exercise_list_sorted[input].amount_of_reps -= exercise_list_sorted[input].addition;
+    } else {
+        exercise_list_sorted[input].base_weight -= exercise_list_sorted[input].addition;
     }
 }
