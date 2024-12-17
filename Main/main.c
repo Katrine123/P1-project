a3a1e68a1af61a5699125e8b844b9ccee404ea45
FreshLuna <msanda24@student.aau.dk>
2024-12-17 18:27:34 +0100

    upgr dgr virker plus logik

--- Main/main.c
+++ Main/main.c
@@ -15,26 +15,20 @@ int main(void) {
 
     // Does save data exist?
     if (check_for_save()) {   // TODO: found_save is a boolean int updated when loading save data
-
         // Evaluation questionnaire
         get_user_data(&user);
         update_possible_exercises(&user);
-        initialize_upgr_dogr(&user);
         int input[user.possible_exercises_count];
         evaluation_questionnaire(&user, input);
         update_routine_workouts(&user);
         print_routine(&user);
-
     } else {
-
         // New user questionnaire
         update_questionnaire(&user);
         update_possible_exercises(&user);
         update_routine_workouts(&user);
         print_routine(&user);
         initialize_upgr_dogr(&user);
-
     }
-
     return 0;
 }
